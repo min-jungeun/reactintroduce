b79e4007f4dd56fac2f1d2ebaf53c96641df523c
min-jungeun <mje971115@gmail.com>
2024-07-04 09:47:03 +0900

    자기소개만들기

---
+++ src/Introduce.js
@@ -0,0 +1,12 @@
+function Mje(){
+    return(
+        // jsx html 태그 아님 xml임 (데이터임)
+        <>
+            <p>내 인생의 첫 리액트</p>
+            <p>내 인생의 첫 리액트</p>
+        </>
+        
+    )
+}
+
+export default Mje;
