bafe06d466ae05f3aa8ea56a0d1643ac13750be0
min-jungeun <mje971115@gmail.com>
2024-07-04 12:34:05 +0900

    로고 + 상단 네비

--- src/component/Hd.js
+++ src/component/Hd.js
@@ -1,11 +1,16 @@
 import logo from '../img/logo.svg'
+import hdscss from './scss/hd.module.scss'
 
 function Hd() {
     return (
-        <header>
+        <header className={hdscss.logo}>
             <div className="container">
-                <h1><a href=""><img src="/img/logo.png" alt="" /></a></h1>
                 <h1><a href=""><img src={logo} alt="" /></a></h1>
+                <ul>
+                    <li>포트폴리오</li>
+                    <li>인물탐방</li>
+                    <li>사전인터뷰 및 제안</li>
+                </ul>
             </div>
             
         </header>
